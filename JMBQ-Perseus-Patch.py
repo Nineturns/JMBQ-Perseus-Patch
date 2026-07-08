a2e8e3761511f487259b4e220f591af3d5011580
Nineturns <zheusemail@gmail.com>
2026-07-09 00:10:03 +0800

    modify method of download JMBQ

--- JMBQ-Perseus-Patch.py
+++ JMBQ-Perseus-Patch.py
@@ -99,7 +99,8 @@ def download_jmbq_perseus_lib():
     repo_owner = "JMBQ"
     repo_name = "azurlane"
     asset_pattern = "MOD_MENU_"
-    mod_dir = Path("mod")
+    mod_dir = Path("patch")
+    extract_dir = Path("JMBQ-PerseusLib")
     mod_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest"
 
     suffix_to_cmd = {
